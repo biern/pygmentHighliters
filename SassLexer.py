a683338d136d5e4e96c8ed795001e569ec4d1ff9
biern <biern.m@gmail.com>
2009-09-19 13:28:40 +0200

    Fixed email in licence

--- SassLexer.py
+++ SassLexer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #pygmentHighliters - third party pygments lexers.
 
-#Copyright (C) 2009 Marcin Biernat <biern.m.com>
+#Copyright (C) 2009 Marcin Biernat <biern.m@gmail.com>
 
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU Affero General Public License as
